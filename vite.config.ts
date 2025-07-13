88527d10327dc4201a95bcd78e9fc7bdf8b5b90c
chuayupeng <yupeng@u.nus.edu>
2025-07-13 22:12:31 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -5,7 +6,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: './',
   server: {
     host: "::",
     port: 8000,
@@ -14,28 +14,10 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === 'development' &&
     componentTagger(),
-    {
-      name: 'replace-tsx-in-html',
-      transformIndexHtml(html) {
-        return html.replace(
-          'src="/src/main.tsx"',
-          'src="./assets/main.js"'
-        );
-      }
-    }
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]'
-      }
-    }
   },
 }));
